4a61b1245b7d3cb544a8d83d57ffecbac0aec09e
Priya Palaniselvaraj <priyaainfo@gmail.com>
2020-04-30 20:04:38 +0530

    Added basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
 
+// sets up routes constant where you define your routes
+const routes: Routes = [
+  { path: 'first-component', component: FirstComponent },
+  { path: 'second-component', component: SecondComponent },
+];
 
-const routes: Routes = [];
-
+// configures NgModule imports and exports
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
