15aa53284664b5f739400122cce7338befb2d6ae
Priya Palaniselvaraj <priyaainfo@gmail.com>
2020-05-01 13:51:38 +0530

    added route navigation from a comp

--- src/app/second/second.component.ts
+++ src/app/second/second.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-second',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SecondComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: Router) { }
 
   ngOnInit() {
+  }
+
+  goto() {
+    this.route.navigate(['first-component']);
+  }
+
+  gotoByUrl() {
+    this.route.navigateByUrl('/first-component');
   }
 
 }
